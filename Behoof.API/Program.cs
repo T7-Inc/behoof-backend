976fb4f7ff45c2cfbb28a2023768549337c1a2a4
oleksii.vakhniuk <oleksii.vakhniuk@sapient.pro>
2023-03-20 00:28:11 +0200

    Added configuration property to AddUserAccessManagement extension

--- Behoof.API/Program.cs
+++ Behoof.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.AddUserAccessManagement();
+builder.AddUserAccessManagement(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
