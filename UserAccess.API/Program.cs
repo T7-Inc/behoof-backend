0c46a37d91084eaa6b680fab6b28a148eded09a5
S8ight <fs.v1835@gmail.com>
2023-03-19 17:05:35 +0200

    Added UserModel and UserDbContext.

--- UserAccess.API/Program.cs
+++ UserAccess.API/Program.cs
@@ -1,12 +1,28 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using UserAccess.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 builder.Services.AddControllers();
 builder.AddUserAccessManagement();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ UserAccess.DAL/DbContext/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserAccess.DAL.Entities;
+
+namespace UserAccess.DAL.DbContext;
+
+public class UserDbContext : IdentityDbContext<User>
+{
+    public UserDbContext(DbContextOptions options)
+        : base(options)
+    {
+        
+    }
+    
+    
+}
