00f3106ef630d56b377dc075bd27297742f02ec9
S8ight <fs.v1835@gmail.com>
2023-04-02 16:55:52 +0300

    Added product projects.

---
+++ ProductsManagement.API/Program.cs
@@ -0,0 +1,28 @@
+using ProductsManagement.API.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.AddProductsManagement(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ProductsManagement.DAL/Data/ProductsDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsManagement.DAL.Data;
+
+public class ProductsDbContext : DbContext
+{
+    public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
+        : base(options)
+    {
+    }
+}
